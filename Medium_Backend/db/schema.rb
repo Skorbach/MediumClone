a663177a9a34214bd794edde6fafa83b2cc4e4fc
Skorbach <dansdesignwork@gmail.com>
2015-07-21 13:01:35 -0400

    Before adding controllers

--- Medium_Backend/db/schema.rb
+++ Medium_Backend/db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721143806) do
+ActiveRecord::Schema.define(version: 20150721152241) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   create_table "profiles", force: :cascade do |t|
