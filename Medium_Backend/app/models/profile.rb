fcfd2d7244b2edc0905d98702a9edcd4233a5e7b
Skorbach <dansdesignwork@gmail.com>
2015-07-21 10:39:41 -0400

    Created Post model and migrated

--- Medium_Backend/app/models/profile.rb
+++ Medium_Backend/app/models/profile.rb
@@ -1,2 +1,12 @@
 class Profile < ActiveRecord::Base
+
+	belongs_to :user
+	has_many :posts
+	
+	validates :name, :presence => true
+	validates :name, :length {
+    	minimum: 2,
+    	maximum: 20
+  	}
+
 end
--- Medium_Backend/db/schema.rb
+++ Medium_Backend/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720215523) do
+ActiveRecord::Schema.define(version: 20150721143806) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.integer  "user_id"
